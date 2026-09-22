50e5bcb3beec7ef050956891fdaf3d138b711f82
siva-0910 <sivaramakrishnaperuboina4@gmail.com>
2026-09-22 09:56:49 +0530

    terraform_module

---
+++ output.tf
@@ -0,0 +1,9 @@
+# output "aws_availability" {
+#     value = slice(data.aws_availability_zones.available.names, 0, 2)
+# }
+# output vpc_id {
+#     value = module.roboshop.vpc_id
+# }
+# output "aws_availability_zone" {
+#     value = module.roboshop.zone_availability
+# }
---
+++ var.tf
@@ -0,0 +1,23 @@
+# variable "cidr_block" {
+#     default = "10.0.0.0/16"
+#   }
+# variable "project" {
+#     default = "roboshop"
+#   }
+# variable "environment" {
+#     default = "dev"
+#   }
+# variable "commponent"{
+#     default = "vpc"
+# }  
+# variable "puilc_sunet_cidr"{
+#     default = ["10.0.1.0/24","10.0.2.0/24"]
+# }
+# variable "private_sunet_cidr"{
+#     default = ["10.0.11.0/24","10.0.12.0/24"]
+# }
+# variable "database_sunet_cidr"{
+#     default = ["10.0.21.0/24","10.0.22.0/24"]
+# }
+
+
